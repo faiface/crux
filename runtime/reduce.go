0aa24cdf9b20ebd13c7b3a141cc19ea94e782646
faiface <faiface@localhost.localdomain>
2018-08-09 22:43:15 +0200

    runtime: rework, polish, and slightly optimize Reduce

--- runtime/reduce.go
+++ runtime/reduce.go
@@ -1,8 +1,23 @@
 package runtime
 
-var Reductions = 0
+var (
+	Reductions = 0
+	MaxStack   = 0
+)
 
-func Reduce(globals []Value, value Value, stack ...Value) Value {
+func Reduce(globals []Value, value Value) (result Value) {
+	var (
+		stack  []Value
+		shares []*Thunk
+	)
+
+	defer func() {
+		for _, share := range shares {
+			share.Result = result
+		}
+	}()
+
+beginning:
 	switch v := value.(type) {
 	case *Char, *Int, *Float, *Struct:
 		if len(stack) > 0 {
@@ -14,76 +29,81 @@ func Reduce(globals []Value, value Value, stack ...Value) Value {
 		if v.Result != nil {
 			return v.Result
 		}
-		if v.Code == nil && v.Data == nil {
+		if v.Code == nil {
 			panic("infinite reduction")
 		}
+
 		code, data := v.Code, v.Data
 		if len(stack) == 0 {
+			shares = append(shares, v)
 			v.Code, v.Data = nil, nil
 		}
-		result := reduceCodeValue(globals, code, data, stack...)
-		if len(stack) == 0 {
-			v.Result = result
+
+		for {
+			Reductions++
+
+			switch code.Kind {
+			case CodeValue:
+				return code.Value
+
+			case CodeOperator:
+				switch operatorArity[code.X] {
+				case 1:
+					return operator1(globals, code.X, stack[len(stack)-1])
+				case 2:
+					return operator2(globals, code.X, stack[len(stack)-1], stack[len(stack)-2])
+				default:
+					panic("invalid arity")
+				}
+
+			case CodeMake:
+				values := make([]Value, len(stack))
+				copy(values, stack)
+				return &Struct{Index: code.X, Values: values}
+
+			case CodeVar:
+				index := int32(len(data)) - code.X - 1
+				value = data[index]
+				goto beginning
+
+			case CodeGlobal:
+				value = globals[code.X]
+				goto beginning
+
+			case CodeAbst:
+				if int32(len(stack)) < code.X {
+					panic("not enough arguments on stack")
+				}
+				pop := int32(len(stack)) - code.X
+				data = make([]Value, code.X)
+				copy(data, stack[pop:])
+				stack = stack[:pop]
+				code = &code.Table[0]
+
+			case CodeAppl:
+				for i := len(code.Table) - 1; i >= 1; i-- {
+					switch code.Table[i].Kind {
+					case CodeValue:
+						stack = append(stack, code.Table[i].Value)
+					case CodeVar:
+						index := int32(len(data)) - code.Table[i].X - 1
+						stack = append(stack, data[index])
+					default:
+						stack = append(stack, &Thunk{Code: &code.Table[i], Data: data})
+					}
+				}
+				code = &code.Table[0]
+
+			case CodeSwitch:
+				str := Reduce(globals, &Thunk{Code: &code.Table[0], Data: data}).(*Struct)
+				stack = append(stack, str.Values...)
+				code = &code.Table[str.Index+1]
+			}
+
+			if len(stack) > MaxStack {
+				MaxStack = len(stack)
+			}
 		}
-		return result
 	}
 	panic("unreachable")
-}
-
-func reduceCodeValue(globals []Value, code *Code, data []Value, stack ...Value) Value {
-	for {
-		Reductions++
-
-		switch code.Kind {
-		case CodeValue:
-			return code.Value
-
-		case CodeOperator:
-			switch operatorArity[code.X] {
-			case 1:
-				return operator1(globals, code.X, stack[len(stack)-1])
-			case 2:
-				return operator2(globals, code.X, stack[len(stack)-1], stack[len(stack)-2])
-			}
-
-		case CodeMake:
-			return &Struct{Index: code.X, Values: stack}
-
-		case CodeVar:
-			index := int32(len(data)) - code.X - 1
-			return Reduce(globals, data[index], stack...)
-
-		case CodeGlobal:
-			return Reduce(globals, globals[code.X], stack...)
-
-		case CodeAbst:
-			if int32(len(stack)) < code.X {
-				panic("not enough arguments on the stack")
-			}
-			pop := int32(len(stack)) - code.X
-			data = make([]Value, code.X)
-			copy(data, stack[pop:])
-			stack = stack[:pop]
-			code = &code.Table[0]
-
-		case CodeAppl:
-			for i := len(code.Table) - 1; i >= 1; i-- {
-				switch code.Table[i].Kind {
-				case CodeValue:
-					stack = append(stack, code.Table[i].Value)
-				case CodeVar:
-					index := int32(len(data)) - code.Table[i].X - 1
-					stack = append(stack, data[index])
-				default:
-					stack = append(stack, &Thunk{Code: &code.Table[i], Data: data})
-				}
-			}
-			code = &code.Table[0]
-
-		case CodeSwitch:
-			str := reduceCodeValue(globals, &code.Table[0], data).(*Struct)
-			stack = append(stack, str.Values...)
-			code = &code.Table[str.Index+1]
-		}
-	}
 }
